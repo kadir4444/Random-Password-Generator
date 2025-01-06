ae4fe4c18fa5f41d9174f7c3594ff2a572d1c88e
kadir4444 <kadir4444@live.co.uk>
2025-01-06 20:52:49 +0000

    v2.0.0 - Initial GUI commit
    - This is the initial GUI input with no backend features deployed at the moment.

--- Random_Password_Generator/Graphical_UI.py
+++ Random_Password_Generator/Graphical_UI.py
@@ -1,5 +1,10 @@
+import random
+import string
 import tkinter
+import traceback
+from operator import truediv
 from tkinter import *
+from tkinter import messagebox
 
 import tk
 
@@ -29,6 +34,17 @@ def on_focusout(event):
 
 # root.config(background="Grey")
 # root.resizable(None, None)
+
+def check_Min_num(min_val):
+    if min_val.isdigit():
+        return True
+    return False
+
+
+def check_Max_num(max_val):
+    if max_val.isdigit():
+        return True
+    return False
 
 
 def createGUI():
@@ -111,29 +127,117 @@ def createGUI():
     min_txt.grid(row=0, column=1, padx=0, pady=1, sticky=W)
 
     max_txt = tkinter.Entry(config_frame,
+                            validatecommand="",
                             width=15,
                             justify="center",
                             relief="sunken",
                             font=("MS Sans Serif", 15))
     max_txt.grid(row=1, column=1, padx=0, pady=1, sticky=W)
 
+    def generatePassword():
+        '''
+        print("Generate Password")
+        mintxt = min_txt.get()
+        maxtxt = max_txt.get()
+        print(mintxt)
+        print(str(maxtxt))
+        '''
+
+        min_length = min_txt.get()
+        max_length = max_txt.get()
+
+        #if max_length.is
+
+        if not check_Min_num(min_length) and not int(min_length):
+            print("Min Value is: " + min_length + "please enter valid number")
+            messagebox.showerror("Minimum Length: Invalid Input",
+                                 "Please input a valid number in the minimum length field.")
+
+        elif not check_Max_num(max_length) and not int(max_length):
+            print("Min Value is: " + max_length + "please enter a valid number")
+            messagebox.showerror("Maximum Length: Invalid Input",
+                                 "Please input a valid number in the maximum length field.")
+
+        else:
+
+            """ Character types """
+            ltr_low = list(string.ascii_lowercase)
+            ltr_up = list(string.ascii_uppercase)
+            nums = list(string.digits)
+            sp_chars = list(string.punctuation)
+
+            """ Password Length Parameters """
+            password_len = random.randint(int(min_length), int(max_length))
+            ltr_low_len = len(ltr_low)
+            ltr_up_len = len(ltr_up)
+            nums_len = len(nums)
+            sp_chars_len = len(sp_chars)
+
+            try:
+                passwrd_char_type = random.randint(0, 3)
+                password = []
+
+                print("Array Created.....")
+                print(password)
+
+                print("Password Length: " + str(password_len))
+
+                pswrd_len = str(password_len)
+                addPassLen(pswrd_len)
+
+                x = 0
+
+                while x < password_len:
+                    passwrd_char_type = random.randint(0, 3)
+
+                    if passwrd_char_type == 0:
+                        password.append(ltr_low[random.randint(0, ltr_low_len - 1)])
+
+                    elif passwrd_char_type == 1:
+                        password.append(ltr_up[random.randint(0, ltr_up_len - 1)])
+
+                    elif passwrd_char_type == 2:
+                        password.append(nums[random.randint(0, nums_len - 1)])
+
+                    elif passwrd_char_type == 3:
+                        password.append(sp_chars[random.randint(0, sp_chars_len - 1)])
+
+                    else:
+                        password.append("# ERROR #")
+                    x += 1
+
+                print(password)
+                passwrd_str = ''.join([str(s) for s in password])
+
+                print(passwrd_str)
+
+            except (ValueError, RuntimeError, TypeError, NameError):
+                traceback.print_exc()
+                print(Exception)
+
     generate_btn = tkinter.Button(config_frame,
                                   width=15,
                                   text="Generate",
+                                  command=generatePassword,
                                   bg="light green",
                                   borderwidth=5,
                                   justify="center",
-                                  relief="sunken",
+                                  relief="raised",
                                   font=("MS Sans Serif", 13))
     generate_btn.grid(row=2, column=0, padx=5, pady=1, sticky=W)
+
+    def clearParameters():
+        min_txt.delete(0, END)
+        max_txt.delete(0, END)
 
     clear_btn = tkinter.Button(config_frame,
                                width=15,
                                text="Clear",
+                               command=clearParameters,
                                bg="light green",
                                borderwidth=5,
                                justify="center",
-                               relief="sunken",
+                               relief="raised",
                                font=("MS Sans Serif", 13))
     clear_btn.grid(row=2, column=1, padx=5, pady=1, sticky=W)
 
@@ -150,7 +254,7 @@ def createGUI():
                              bg="light green",
                              borderwidth=5,
                              justify="center",
-                             relief="sunken",
+                             relief="raised",
                              font=("MS Sans Serif", 13))
     log_btn.grid(row=3, column=0, padx=5, pady=1, sticky=W)
 
@@ -177,6 +281,12 @@ def createGUI():
                                  font=("MS Sans Serif", 10))
     pass_len_lbl.grid(row=0, column=1, padx=0, pady=1, sticky=W)
 
+    def addPassLen(array_length):
+        if array_length.isdigit():
+            pass_len_lbl.config(text=array_length)
+            return True
+        return False
+
     inner_generator_frame = tkinter.Frame(generator_frame)
     inner_generator_frame.grid(row=1, column=0)
 
@@ -189,17 +299,26 @@ def createGUI():
                              font=("MS Sans Serif", 15))
     pass_txt.grid(row=0, column=0, padx=0, pady=1, sticky=W)
 
-    def clearSetting():
-        print("ll")
+    def inputPassword(passwrd):
+        if passwrd.contains(""):
+            pass_txt.insert(str(passwrd))
+            return True
+        return False
+
+
+    def resetPasswordSetting():
+        print("Clear Password")
+        pass_len_lbl.config(text="-" * 30)
+        pass_txt.delete(0, END)
 
     reset_pass_btn = tkinter.Button(inner_generator_frame,
                                     width=30,
-                                    command=clearSetting,
+                                    command=resetPasswordSetting,
                                     text="Reset Password",
                                     bg="light green",
                                     borderwidth=5,
                                     justify="center",
-                                    relief="sunken",
+                                    relief="raised",
                                     font=("MS Sans Serif", 14))
     reset_pass_btn.grid(row=1, column=0, padx=0, pady=1, sticky=W)
 
